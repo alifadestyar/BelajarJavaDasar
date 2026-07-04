67af84e0d1acba2acd2c3619891343f58beef601
alifadestyar <alifadestyar12@gmail.com>
2026-07-04 15:04:58 +0700

    Pertemuan 10

---
+++ src/main/java/com/mycompany/belajarjavadasar/pertemuan10/RunBank.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.belajarjavadasar.pertemuan10;
+
+/**
+ *
+ * @author User
+ */
+public class RunBank {
+    public static void main(String[] args){
+        RekeningBank rek = new RekeningBank("123456", 1000000);
+        
+        rek.tarik(500000);
+        rek.setor(200000);
+        rek.tarik(1000000);
+        
+        System.out.println("Saldo akhir: Rp " + rek.getSaldo());
+    }
+}
